ccbffa891c84dd41cb4d1743565d5bdac018fb56
allsunnyday <jeonghyunsun23@gmail.com>
2018-10-12 01:48:29 +0900

    today commit

--- src/main/java/com/planit/backend/web/BbsController.java
+++ src/main/java/com/planit/backend/web/BbsController.java
@@ -28,27 +28,8 @@ public class BbsController {
 	
 
 	@RequestMapping("/Planit/Admin/BBS/AskList.do")
-	public String gotoAskList(@RequestParam Map map,  // 검색을 설정하기 위한
-										Model model		// 데이터를 보내기 위한 모델
+	public String gotoAskList(@RequestParam Map map	// 데이터를 보내기 위한 모델
 													)throws Exception{
-		// 서비스 호출 
-		// u_ask_planit  -- 리스트 및  새로운 글 개수 
-		/*List<UserAskDTO> userAsks = askService.selectUserAskList(map);
-		int uWatiedNumber=0;
-		for(UserAskDTO dto: userAsks) {
-			if(dto.getStatus().equals("waited"))
-				uWatiedNumber++;
-		}
-		
-		// p_ask_planit  -- 새로운 글 새수만 
-		map.put("status", "waited");
-		List<Map> partnerAsks = askService.selectPartnerAskList(map); 
-		System.out.println(partnerAsks.size()+"개 있습니다.");
-		
-		// 데이터 저장
-		model.addAttribute("userAsk", userAsks);
-		model.addAttribute("uWatiedNumber", uWatiedNumber);
-		model.addAttribute("pWatiedNumber", partnerAsks.size()); */ 
 		
 		//리스트 화면에서는 기본적으로 사용자 문의 게시판이 먼저 보인다.
 		return "bbs/AskBbsList.tiles";
